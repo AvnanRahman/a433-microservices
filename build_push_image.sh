a7c322262fbf3b08be991c99a856868dab09002b
Afnan Ngathour Rahman <avnanrahman@gmail.com>
2024-02-21 13:10:36 +0700

    change image name

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,20 +1,20 @@
 #Variable
-IMAGE="hiroominan/microservices-proyek-pertama"
-APP="item-app"
+DOCKER_HUB_IMG="hiroominan/item-app"
+LOCAL_IMG="item-app"
+TAG="v1"
 
 #Remove Previous Build
-docker rmi $APP:v1
+docker rmi $LOCAL_IMG:$TAG
 
 #Build image
 cd ~/proyek-pertama
-sudo docker build -t $APP:v1 .
+sudo docker build -t $LOCAL_IMG:$TAG .
 #See list images
 docker images
 #Memberi nama baru ke image/tag
-docker image tag $APP:v1 $IMAGE:v1
+docker image tag $LOCAL_IMG:$TAG $DOCKER_HUB_IMG:$TAG
 
 #login ke docker hub
 cat ~/proyek-pertama/docker_token.txt | docker login -u hiroominan --password-stdin
 #push image ke docker hub
-docker push $IMAGE:v1
-docker rmi $IMAGE:v1
+docker push $DOCKER_HUB_IMG:$TAG
