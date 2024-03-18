62e313b681ff83a7060d8c2b9da20b4670a2dbf4
Afnan Ngathour Rahman <avnanrahman@gmail.com>
2024-03-19 01:17:44 +0700

    feat: add CI and Script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Set variable tentang images
+image_name="karsajobs"
+image_version="latest"
+username_github="avnanrahman"
+package_name="$username_github/$image_name:$image_version"
+
+# Build docker image
+echo -e "Building backend karsajobs images..."
+docker build . -t $package_name
+
+# Set tag baru untuk image yang telah di build
+echo -e "Set image tag sesuai format GitHub Container Registry..."
+docker tag $package_name ghcr.io/$package_name
+
+# List available images
+echo -e "Printing list of Docker Images..."
+docker images
+
+# Login ke github CR
+echo -e "Login ke Github CR..."
+docker login ghcr.io -u $username_github -p $GCR_TOKEN
+
+# Push image ke github CR
+echo -e "Push Image ke Github CR..."
+docker push ghcr.io/$package_name
+
+echo -e "\nDone."
