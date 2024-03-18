9cc71d4961e63c79210b8ec11717f51231c67140
Afnan Ngathour Rahman <avnanrahman@gmail.com>
2024-03-19 00:29:30 +0700

    feat: add ci

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Set variable tentang images
+image_name="karsajobs-ui"
+image_version="latest"
+username_github="afnanrahman"
+package="$username_github/$image_name:$image_version"
+
+# Build docker image
+echo -e "Building frontend karsajobs-ui images..."
+docker build -t . $package
+
+# Set tag baru untuk image yang telah di build
+echo -e "Set image tag sesuai format GitHub Container Registry..."
+docker tag $package_name ghcr.io/$package_name
+
+# List available images
+echo -e "Printing list of Docker Images"
+docker images
+
+# Login ke github CR
+docker login ghcr.io -u $username_github -p $GCR_TOKEN
+
+# Push image ke github CR
+docker push ghcr.io/$package_name
+
+echo -e "\nDone."
